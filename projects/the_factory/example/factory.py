accabd274cbcb733d03f48ecdf2fb0811e5c1e9a
Ben Bistline <bbistline02@gmail.com>
2021-02-04 21:08:47 -0500

    began the factory project.

---
+++ projects/the_factory/example/factory.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+This class is the head class. The management system. 
+The overall grand daddy. From here, we have the building management
+(including all the machines), the finance manager, and 
+the employment directory. 
+"""
+
+pass
+
+class CocaColaFactory:
+
+    def __init__(self):
+        pass
