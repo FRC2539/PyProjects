64803f8dad37014c62c4bb507dae79733e6f278d
Ben Bistline <bbistline02@gmail.com>
2021-02-15 19:25:18 -0500

    Randy did this project

---
+++ projects/loops/prime_numbers/randyn.py
@@ -0,0 +1,15 @@
+prime = int(input("Enter value: "))
+
+i = 2
+if (prime < 0):
+    print("not prime");
+    quit()
+while (i < prime):
+    if (prime % i == 0):
+        print("not prime")
+        quit()
+        
+    else:
+        i = i + 1
+        
+print("prime")
